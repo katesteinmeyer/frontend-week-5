3007bb94a05cd4516784d4975357acb5d436765f
katesteinmeyer <katesteinmeyer@gmail.com>
2015-09-09 21:07:42 -0700

    We added to-list form

--- code-appetizer/script.js
+++ code-appetizer/script.js
@@ -1,6 +1,16 @@
-/*
 
-  Your Javascript here
 
-*/
 
+  function pictureChange()
+    {
+          document.getElementById("theImage").src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/54/Golden_Gate_Bridge_0002.jpg/1024px-Golden_Gate_Bridge_0002.jpg";
+   
+
+   if  () {
+          document.getElementById("theImage").src="https://upload.wikimedia.org/wikipedia/commons/5/57/Golden_gate_circa_1891.png";
+
+    } else if () {
+          document.getElementById("theImage").src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/54/Golden_Gate_Bridge_0002.jpg/1024px-Golden_Gate_Bridge_0002.jpg";
+
+    }
+}
--- exercise-1/script.js
+++ exercise-1/script.js
@@ -1,5 +1,26 @@
-/*
 
-  Your Javascript here
+function addFancyList(toDo) {
 
-*/
+    var table = document.getElementById(toDo);
+
+        var rowCount = table.rows.length;
+            var row = table.insertRow(rowCount);
+
+          var b=document.getElementById('txt1').value;
+ var cell1 = row.insertCell(0);
+            var element1 = document.createElement("input");
+            element1.type = "text";
+            element1.style.width="100%";
+            element1.name = "txtbox[]";
+            element1.value=b;
+                   cell1.appendChild(element1);
+ var cell2 = row.insertCell(1);
+            var element2 = document.createElement("input");
+            element2.type = "checkbox";
+            element2.name="chkbox[]";
+            cell2.appendChild(element2);
+document.getElementById('txt1').value=" ";
+
+
+}
+
